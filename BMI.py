8f2cb92cc2f04892d166dfc8e465b666595ef871
nikhilmuneshwar0 <nikhilmuneshwar@yahoo.com>
2022-04-01 16:45:30 +0300

    Update BMI.py
    Complete and working

--- BMI.py
+++ BMI.py
@@ -4,4 +4,9 @@ weight = input("enter your weight in kg: ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-print(type(height))
+# print(type(height))
+w1 = float(weight)
+h1 = float(height)
+bmi = w1/(h1*h1)
+bmi1 = int(bmi)
+print(bmi1)
