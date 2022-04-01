5a92674375507c817092be494fba10507d7faa5b
nikhilmuneshwar0 <96412735+nikhilmuneshwar0@users.noreply.github.com>
2022-04-01 16:54:29 +0300

    Add files via upload

---
+++ numberAddition.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+firstNumber = two_digit_number[0]
+secondNumber = two_digit_number[1]
+# print(type(firstNumber))
+# print(firstNumber)
+# print(secondNumber)
+fNumber = int(firstNumber)
+sNumber = int(secondNumber)
+result = fNumber + sNumber
+print(result)
