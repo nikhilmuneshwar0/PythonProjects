a526b99d63ea923a3992c307b08dac105c94b6bb
nikhilmuneshwar0 <96412735+nikhilmuneshwar0@users.noreply.github.com>
2022-04-01 16:50:08 +0300

    Add files via upload

---
+++ addDigits.py
@@ -0,0 +1,19 @@
+
+two_digit_number = input("Type a two digit number: ")
+
+#print(type(two_digit_number))
+
+#twoNumber = int(two_digit_number)
+#print(type(two_digit_number))
+firstNumber = two_digit_number[0]
+secondNumber = two_digit_number[1]
+# print(type(firstNumber))
+# print(firstNumber)
+# print(secondNumber)
+fNumber = int(firstNumber)
+sNumber = int(secondNumber)
+result = fNumber + sNumber
+print(result)
+
+
+
